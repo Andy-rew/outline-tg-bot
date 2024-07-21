57a0ae164cebcf7870f00d5591b9d17860188a6d
TimkaMLG <muradov.stfpmi@gmail.com>
2024-07-22 11:09:59 +1200

    fixed tabs

--- code/main.py
+++ code/main.py
@@ -77,65 +77,70 @@ async def new_key_callback(message: types.Message) -> None:
                        parse_mode='Markdown',
                        )
 
-    @bot.message_handler(func=check_admin, commands=['delete_key'])
-    async def delete_key_buttons(message: types.Message) -> None:
-        markup = types.InlineKeyboardMarkup(row_width=1)
 
-        keys = client.get_keys()
+@bot.message_handler(func=check_admin, commands=['delete_key'])
+async def delete_key_buttons(message: types.Message) -> None:
+    markup = types.InlineKeyboardMarkup(row_width=1)
 
-        buttons = []
-        for key in keys:
-            button = types.InlineKeyboardButton(
-                f"ID: {key.key_id}, Name: {key.name}",
-                callback_data=key.key_id)
-            buttons.append(button)
-        markup.add(*buttons)
+    keys = client.get_keys()
 
-        cancel_button = types.InlineKeyboardButton(f"Cancel",
-                                                   callback_data=None)
-        markup.add(cancel_button)
+    buttons = []
+    for key in keys:
+        button = types.InlineKeyboardButton(
+            f"ID: {key.key_id}, Name: {key.name}",
+            callback_data=key.key_id)
+        buttons.append(button)
+    markup.add(*buttons)
 
-        await bot.send_message(message.chat.id, "Choose key for deletion",
-                               reply_markup=markup)
+    cancel_button = types.InlineKeyboardButton(f"Cancel",
+                                               callback_data=None)
+    markup.add(cancel_button)
 
-    @bot.callback_query_handler(func=lambda call: True)
-    async def callback_query(call: types.CallbackQuery) -> None:
-        message = call.message
-        chat_id = message.chat.id
-        message_id = message.message_id
+    await bot.send_message(message.chat.id, "Choose key for deletion",
+                           reply_markup=markup)
 
-        if call.data is None:
-            await bot.edit_message_text(chat_id=chat_id, message_id=message_id,
-                                        text=f'Canceled')
-            await bot.answer_callback_query(call.id, "Canceled")
-            return
 
-        delete_status_ok = client.delete_key(call.data)
-        if delete_status_ok:
-            await bot.edit_message_text(chat_id=chat_id, message_id=message_id,
-                                        text=f'Success deletion\nID: {call.data}')
-            await bot.answer_callback_query(call.id, "Done")
-        else:
-            await bot.edit_message_text(chat_id=chat_id, message_id=message_id,
-                                        text="Failed deletion")
-            await bot.answer_callback_query(call.id, "Failed")
+@bot.callback_query_handler(func=lambda call: True)
+async def callback_query(call: types.CallbackQuery) -> None:
+    message = call.message
+    chat_id = message.chat.id
+    message_id = message.message_id
 
-    @bot.message_handler(commands=['start'])
-    async def start_callback(message: types.Message) -> None:
-        await bot.reply_to(message, 'Welcome to Outline, body!')
+    if call.data is None:
+        await bot.edit_message_text(chat_id=chat_id, message_id=message_id,
+                                    text=f'Canceled')
+        await bot.answer_callback_query(call.id, "Canceled")
+        return
 
-    async def main():
-        commands = [
-            BotCommand(command='start', description='Start'),
-            BotCommand(command='metrics', description='Show statistics'),
-            BotCommand(command='new_key', description='Add new key'),
-            BotCommand(command='delete_key', description='Delete old key'),
-            BotCommand(command='help', description='Get help')
-        ]
+    delete_status_ok = client.delete_key(call.data)
+    if delete_status_ok:
+        await bot.edit_message_text(chat_id=chat_id, message_id=message_id,
+                                    text=f'Success deletion\nID: {call.data}')
+        await bot.answer_callback_query(call.id, "Done")
+    else:
+        await bot.edit_message_text(chat_id=chat_id, message_id=message_id,
+                                    text="Failed deletion")
+        await bot.answer_callback_query(call.id, "Failed")
 
-        await bot.set_my_commands(commands=commands)
 
-        await bot.polling()
+@bot.message_handler(commands=['start'])
+async def start_callback(message: types.Message) -> None:
+    await bot.reply_to(message, 'Welcome to Outline, body!')
 
-    if __name__ == '__main__':
-        asyncio.run(main())
+
+async def main():
+    commands = [
+        BotCommand(command='start', description='Start'),
+        BotCommand(command='metrics', description='Show statistics'),
+        BotCommand(command='new_key', description='Add new key'),
+        BotCommand(command='delete_key', description='Delete old key'),
+        BotCommand(command='help', description='Get help')
+    ]
+
+    await bot.set_my_commands(commands=commands)
+
+    await bot.polling()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
